c90cd29eb2e9cb54d89f443eb6bf83742aa29b0c
dconde <davidcondemartinez1997@gmail.com>
2017-08-22 16:43:14 +0200

    Añadido gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,60 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id="home">
+    <div id="usuarios" v-on:click="openComponent">Usuarios</div>
+    <div id="domicilios" v-on:click="openComponent">Domicilios</div>
+    <div id="cuentas" v-on:click="openComponent">Cuentas bancarias</div>
+
+    <div id="maestro"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  import axios from "axios"
+  import Vue from 'vue'
+  //import MaestroUsuario from "usuarios.vue"
+  //import MaestroDomicilio from "domicilios.vue"
+  //import MaestroCuentas from "cuentas.vue"
+  export default {
+    name: 'app',
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+      openComponent: function(e){
+        switch(e.target.id){
+          case "usuarios": 
+          new Vue({
+            el: '#maestro',
+            render: h => h(App)
+          })
+
+          break;
+          case "domicilios": 
+          new Vue({
+            el: '#maestro',
+            render: h => h(App)
+          })
+
+          break;
+          case "cuentas": 
+          new Vue({
+            el: '#maestro',
+            render: h => h(App)
+          })
+
+          break;
+        }
+      }
+
+    },
+
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  *{
+    padding: 5px;
+  }
 </style>
